1b294e80ff0c8cac5d2ea9b1622bf86ffad1c9d6
goblock1938 <goblock1938@gmail.com>
2024-10-02 12:46:58 +0700

    first commit

---
+++ logic.js
@@ -0,0 +1,112 @@
+function showside() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.right = '0'
+}
+function hideside() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.right = '-250px'
+}
+function bio_1() {
+    const bio = document.querySelector(".bio-1")
+    bio.style.bottom = '0'
+}
+function close_bio_1() {
+    const bio = document.querySelector(".bio-1")
+    bio.style.bottom = '-11cm'
+}
+function bio_2() {
+    const bio = document.querySelector(".bio-2")
+    bio.style.bottom = '0'
+}
+function close_bio_2() {
+    const bio = document.querySelector(".bio-2")
+    bio.style.bottom = '-11cm'
+}
+function bio_3() {
+    const bio = document.querySelector(".bio-3")
+    bio.style.bottom = '0'
+}
+function close_bio_3() {
+    const bio = document.querySelector(".bio-3")
+    bio.style.bottom = '-11cm'
+}
+function bio_4() {
+    const bio = document.querySelector(".bio-4")
+    bio.style.bottom = '0'
+}
+function close_bio_4() {
+    const bio = document.querySelector(".bio-4")
+    bio.style.bottom = '-11cm'
+}
+function bio_5() {
+    const bio = document.querySelector(".bio-5")
+    bio.style.bottom = '0'
+}
+function close_bio_5() {
+    const bio = document.querySelector(".bio-5")
+    bio.style.bottom = '-11cm'
+}
+function bio_6() {
+    const bio = document.querySelector(".bio-6")
+    bio.style.bottom = '0'
+}
+function close_bio_6() {
+    const bio = document.querySelector(".bio-6")
+    bio.style.bottom = '-11cm'
+}
+function bio_7() {
+    const bio = document.querySelector(".bio-7")
+    bio.style.bottom = '0'
+}
+function close_bio_7() {
+    const bio = document.querySelector(".bio-7")
+    bio.style.bottom = '-11cm'
+}
+function bio_8() {
+    const bio = document.querySelector(".bio-8")
+    bio.style.bottom = '0'
+}
+function close_bio_8() {
+    const bio = document.querySelector(".bio-8")
+    bio.style.bottom = '-11cm'
+}
+function bio_9() {
+    const bio = document.querySelector(".bio-9")
+    bio.style.bottom = '0'
+}
+function close_bio_9() {
+    const bio = document.querySelector(".bio-9")
+    bio.style.bottom = '-11cm'
+}
+function bio_10() {
+    const bio = document.querySelector(".bio-10")
+    bio.style.bottom = '0'
+}
+function close_bio_10() {
+    const bio = document.querySelector(".bio-10")
+    bio.style.bottom = '-11cm'
+}
+function bio_11() {
+    const bio = document.querySelector(".bio-11")
+    bio.style.bottom = '0'
+}
+function close_bio_11() {
+    const bio = document.querySelector(".bio-11")
+    bio.style.bottom = '-11cm'
+}
+function bio_12() {
+    const bio = document.querySelector(".bio-12")
+    bio.style.bottom = '0'
+}
+function close_bio_12() {
+    const bio = document.querySelector(".bio-12")
+    bio.style.bottom = '-11cm'
+}
+function bio_13() {
+    const bio = document.querySelector(".bio-13")
+    bio.style.bottom = '0'
+}
+function close_bio_13() {
+    const bio = document.querySelector(".bio-13")
+    bio.style.bottom = '-11cm'
+}
